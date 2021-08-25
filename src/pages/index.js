217dbc658fd559f464b0100c011a6d9bddea4b9f
heyjuwooo <88545528+heyjuwooo@users.noreply.github.com>
2021-08-25 15:50:09 +0900

    a

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,20 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import Header from '../components/header'
 
 const IndexPage = () => (
   <div>
-    <h1>Learn to design and code React apps</h1>
-    <p>와우 짱 신기하다!</p>
-    <Link to="/page-2/">Watch the viedo</Link>
+    <div className="Hero">
+      <div className="HeroGroup">
+      <h1>Learn to <br />design and code React 
+      apps</h1>
+      <p>Complete courses about the best tools and design system. Prototye and build apps with React and Swift.</p>
+      <Link to="/page-2/">Watch the viedo</Link>
+      <svg width="2560" height="172" viewBox="0 0 2560 172" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M0 25.9086C277 84.5821 433 65.736 720 25.9086C934.818 -3.9019 1214.06 -5.23669 1442 8.06597C2079 45.2421 2208 63.5007 2560 25.9088V171.91L0 171.91V25.9086Z" fill="white"/>
+       </svg>
+      </div>
+    </div>
   </div>
 )
 
